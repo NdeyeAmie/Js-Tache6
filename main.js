60af3eed7e739d652338e42c5b56e877a3a123e9
NdeyeAmie <tndeyeamie456@gmail.com>
2024-10-12 15:37:08 +0000

    firt-commit

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 // Générer un nombre aléatoire entre 10 et 100
 // Math.random() en JavaScript génère des nombres pseudo-aléatoires.
+/*Si le nombre est un décimal positif (par exemple 4.7 ou 4.1), 
+Math.floor() retourne l'entier inférieur (respectivement 4 dans les deux cas).
+Si le nombre est négatif, Math.floor() retourne l'entier inférieur le plus proche en dessous.
+ Par exemple, pour -4.7, il retourne -5 (car -5 est plus petit que -4).*/
 let nombreADeviner = Math.floor(Math.random() * 91) + 10;
 let nombreEssais = 10;
 let essais = 0;
