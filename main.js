f1cc9407a26bd176c22b2a5bbd5c4d7b08b0f761
NdeyeAmie <tndeyeamie456@gmail.com>
2024-10-12 15:26:04 +0000

    firt-commit

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // Générer un nombre aléatoire entre 10 et 100
+// Math.random() en JavaScript génère des nombres pseudo-aléatoires.
 let nombreADeviner = Math.floor(Math.random() * 91) + 10;
 let nombreEssais = 10;
 let essais = 0;
@@ -10,14 +11,17 @@ function devinerNombre() {
 
     if (essais <= 10) {
         if (nombrePropose === nombreADeviner) {
-            alert(`Bravo ! Vous avez deviné le nombre en ${essais} essais.`);
+            alert("Bravo ! Vous avez deviné le nombre en" + essais + "essais.");
         } else if (nombrePropose < nombreADeviner) {
-            alert(`Le nombre à deviner est plus grand que ${nombrePropose}. Il vous reste ${nombreEssais - essais} essais.`);
+            alert("Le nombre à deviner est plus grand que" + " " + nombrePropose +  " " +  ". Il vous reste " +  " " + (nombreEssais - essais) + " " + "essais.");
         } else {
-            alert(`Le nombre à deviner est plus petit que ${nombrePropose}. Il vous reste ${nombreEssais - essais} essais.`);
+            alert("Le nombre à deviner est plus petit que" + " " + nombrePropose + " " +  ". Il vous reste" +  " " + (nombreEssais - essais) + " " + "essais.");
         }
     } else {
-        alert(`Vous avez utilisé tous vos essais. Le nombre à deviner était ${nombreADeviner}.`);
+        alert("Vous avez utilisé tous vos essais. Le nombre à deviner était" + " " +  nombreADeviner  + " " + " .");
     }
 }
+
+let nombreAleatoire = Math.floor(Math.random() * 91)+10;
+console.log(nombreAleatoire);
  
